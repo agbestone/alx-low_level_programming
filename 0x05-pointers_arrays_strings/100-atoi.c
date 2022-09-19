6dc8356d64cb095fbe136a5d122d8a3378d9c58a
Auwal Iliyasu Ishaku <gurubestone@gmail.com>
2022-09-19 12:14:37 -0700

    05

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+  int i = 0;
+	while (true)
+	{
+	  if (s[i] != '+' && s[i] != '-' && s[i] != '0' && s[i] != '1' && s[i] != '2' && s[i] != '3' && s[i] != '4' && s[i] != '5' && s[i] != '6' && s[i] != '7' && s[i] != '8' && s[i] != '9')
+	    {
+	      continue;
+	      else
+		{
+		  if (s[i] = '-')
+		    {
+		      ++minu;
+		    }
+		  else if (s[i] = '+')
+		    {
+		      continue;
+		    }
+		  else
+		    
+}
